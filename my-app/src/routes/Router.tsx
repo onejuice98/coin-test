09ec4e8731139527090eb12fce0860170c7fdb89
‍원정수[학생](전자정보대학 전자공학과) <waterpurifier@khu.ac.kr>
2022-07-15 16:12:58 +0000

    browserRouter

--- my-app/src/routes/Router.tsx
+++ my-app/src/routes/Router.tsx
@@ -4,7 +4,7 @@ import Coins from "./Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"coin-test"}>
       <Routes>
         <Route path="/:coinId/*" element={<Coin/>}/>
         <Route path="/" element={<Coins/>} />
